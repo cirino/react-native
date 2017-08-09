eabcbb6b1d49cd9c3484de78241359af0e203393
cirino <dagmar.mv@gmail.com>
2017-08-09 01:10:01 -0300

    Implementando o Router Flux

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
-/*import React, { Component }  from 'react';
-import { Router, Scene } from 'react-native-router-flux';
-
-import FormLogin from './components/FormLogin';
-import FormCadastro from './components/FormCadastro';
-
-export default class App4 extends Component {
-  render() {
-    return (
-      <Router>
-        <Scene key="root">
-          <Scene key="formLogin" component={FormLogin} title="FormLogin" initial={true} />
-          <Scene key="formCadastro" component={FormCadastro} title="FormCadastro" />
-        </Scene>
-      </Router>
-    )
-  }
-}
-*/
-import React, { Component } from 'react';
-import { Router, Scene } from 'react-native-router-flux';
-
-import PageOne from './PageOne';
-import PageTwo from './PageTwo';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import Routes from './Routes';
-import FormLogin from './components/FormLogin';
-import FormCadastro from './components/FormCadastro';
+import reducers from './reducers';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Scene key="root">
-          <Scene key="formCadastro" component={FormCadastro} title="Form Cadastro" initial={true} />
-          <Scene key="pageOne" component={PageOne} title="PageOne" initial={false} />
-          <Scene key="pageTwo" component={PageTwo} title="PageTwo" />
-        </Scene>
-      </Router>
-    )
-  }
-}
+export default props => (
+    <Provider store={createStore(reducers)}>
+        <Routes />
+    </Provider>
+);
--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import { Actions, Scene, Router } from 'react-native-router-flux';
-
-import PageOne from './PageOne';
-import PageTwo from './PageTwo';
+import React from 'react';
+import { Router, Scene } from 'react-native-router-flux';
 import FormLogin from './components/FormLogin';
 import FormCadastro from './components/FormCadastro';
 
-const scenes = Actions.create(
-  <Scene key="root">
-    <Scene key="form_Cadastro" component={FormCadastro} title="Form_Cadastro"/>
-    <Scene key="page_One" component={PageOne} title="Page_One" initial={false} />
-    <Scene key="page_Two" component={PageTwo} title="Page_Two" initial={true}  />
-  </Scene>
-);
-
-class App extends Component {
-  render() {
-    return <Router scenes={scenes}/>
-  }
-}
+export default props => (
+    <Router>
+      <Scene key="root">
+        <Scene key='formLogin' component={FormLogin} title="Login" initial={false} />
+        <Scene key='formCadastro' component={FormCadastro} title="Cadastro" initial={true} />
+      </Scene>
+    </Router>
+)
